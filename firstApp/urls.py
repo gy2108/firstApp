c6ab7310447c3b1c0b5cb5a12d43190562c0fedb
gy2108 <gopalyadav2108@gmail.com>
2019-01-29 04:43:56 +0530

    Implemented Login,Register,Profile,UpdateProfile

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -18,9 +18,18 @@ from django.urls import path
 from hello.views import myView
 from todo.views import todoView, addTodo, deleteTodo
 from blog.views import about,home
+from users import views as users_view
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', users_view.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('profile/', users_view.profile, name='profile'),
     path('sayhello/',myView),
     path('todo/',todoView),
     path('addTodo/',addTodo),
@@ -29,3 +38,6 @@ urlpatterns = [
     path('about/', about , name="blog-about"),
     path('', home , name="blog-home"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
