4c48d9111ee9ebe50f95d75f94331fe09f52e5d5
gy2108 <gopalyadav2108@gmail.com>
2019-02-27 21:29:11 +0530

    profile

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.http import HttpResponse
 from .models import Post
 
@@ -8,6 +9,10 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request , 'blog/home.html' , context)
+
+#class PostListView(ListView):
+   # model = Post
+    #template_name = 'blog/home.html'
 
 def about(request):
     return render(request , 'blog/about.html', {'title':'About'})
